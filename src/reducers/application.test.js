2168f36cb27d4a8461701a95bb714f736788e2e3
Dan <hexular@gmail.com>
2019-12-02 17:22:46 -0500

    add cypress for testing

--- src/reducers/application.test.js
+++ src/reducers/application.test.js
@@ -4,3 +4,12 @@ import reducer, {
   SET_INTERVIEW
 } from "../reducers/application.js";
 import spotCounter from "../helpers/spotCounter";
+import { exportAllDeclaration } from "@babel/types";
+
+describe("Application Reducer", () => {
+  it("thows an error with an unsupported type", () => {
+    expect(() => reducer({}, { type: null })).toThrowError(
+      /tried to reduce with unsupported action type/i
+    );
+  });
+});
