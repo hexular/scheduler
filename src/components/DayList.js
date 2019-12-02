83e3d7a00d38daea02ab21b08875ddfa786e2195
Dan <hexular@gmail.com>
2019-12-02 09:22:37 -0500

    add testing file with basic render test for appointment

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,7 +5,7 @@ export default function DayList(props) {
   const days = props.days.map(day => {
     return (
       <DayListItem 
-      key={day.id}
+          key={day.id}
           name={day.name} 
           spots={day.spots} 
           selected={day.name === props.day}
